125b0318883ec2a560bf8b315ff2ceb8ca2dd73f
Nishchith Rao <nishchith14600@gmail.com>
2025-10-15 16:59:35 -0700

    status code updates for sign-out

--- src/main/java/org/sfa/volunteer/dto/common/SaayamStatusCode.java
+++ src/main/java/org/sfa/volunteer/dto/common/SaayamStatusCode.java
@@ -20,6 +20,7 @@ public enum SaayamStatusCode {
     VOLUNTEER_UPDATED("SAAAYAM-1209"),
     VOLUNTEER_EXISTS("SAAAYAM-1210"),
     ORGANIZATION_UPDATED("SAAAYAM-1211"),
+    USER_SIGNED_OUT("SAAAYAM-1212"),
 
 
     // Client error codes (1300-1349)
