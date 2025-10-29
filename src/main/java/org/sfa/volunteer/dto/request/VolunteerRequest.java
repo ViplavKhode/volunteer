b75380e7b8d7a56cbcb00187779ec4ce63717676
Ramyasahithya <mramyasahithya@gmail.com>
2025-10-29 15:19:21 -0400

    code changes done to volunteer branch based on the database

--- src/main/java/org/sfa/volunteer/dto/request/VolunteerRequest.java
+++ src/main/java/org/sfa/volunteer/dto/request/VolunteerRequest.java
@@ -1,4 +1,5 @@
 package org.sfa.volunteer.dto.request;
+
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import java.time.ZonedDateTime;
@@ -6,15 +7,15 @@ import java.util.List;
 
 @Builder
 public record VolunteerRequest(
-        @NotBlank Integer step,
-        @NotBlank String userId,
-        Boolean termsAndConditions,
-        ZonedDateTime tcUpdateDate,
-        String govtIdFilename,
-        ZonedDateTime govtUpdateDate,
-        String skills,
-        Boolean notification,
-        Boolean isCompleted,
-        ZonedDateTime completedDate,
-        List<VolunteerUserAvailabilityRequest> availability) {
+                @NotBlank Integer step,
+                @NotBlank String userId,
+                Boolean termsAndConditions,
+                ZonedDateTime tcUpdateDate,
+                String govtIdFilename,
+                ZonedDateTime govtUpdateDate,
+                List<String> skills,
+                Boolean notification,
+                Boolean isCompleted,
+                ZonedDateTime completedDate,
+                List<VolunteerUserAvailabilityRequest> availability) {
 }
