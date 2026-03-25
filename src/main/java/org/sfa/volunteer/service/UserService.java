1564c43a9214d43f454678d3ff8025c83858569a
Srishti Ahirwar <144982823+srishti772@users.noreply.github.com>
2026-03-25 19:16:48 -0400

    Revert "Srishti ahirwar preference page issue120"

--- src/main/java/org/sfa/volunteer/service/UserService.java
+++ src/main/java/org/sfa/volunteer/service/UserService.java
@@ -3,8 +3,7 @@ package org.sfa.volunteer.service;
 import org.sfa.volunteer.dto.request.CreateUserRequest;
 import org.sfa.volunteer.dto.request.UpdateOrganizationRequest;
 import org.sfa.volunteer.dto.request.UpdateUserProfileRequest;
-import org.sfa.volunteer.dto.request.UserPreferenceRequest;
-import org.sfa.volunteer.dto.response.UserPreferenceResponse;
+
 import org.sfa.volunteer.dto.response.*;
 
 
@@ -38,6 +37,4 @@ public interface UserService {
     java.util.Optional<String> getProfilePicturePath(String userId);
     boolean userExists(String userId);
     String getUserIdByEmailForAuth(String email);
-
-    UserPreferenceResponse updateUserPreferences(String userId, UserPreferenceRequest request) throws Exception;
 }
